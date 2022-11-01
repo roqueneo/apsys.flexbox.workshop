2efd01749cf8a8fd766369802bb332ea5aa002ac
Alonso Roque <roqueneo@gmail.com>
2022-11-01 15:37:18 -0600

    Set base styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='header'>
+				<h1>Learning Flexbox</h1>
+			</header>
+			<body className='body'>
+				<div className='container'>
+					<div className='home'>Home</div>
+					<div className='search'>Search</div>
+					<div className='logout'>Logout</div>
+				</div>
+				<div className='container'>
+					<div class='item first-face'>
+						<span class='pip'></span>
+					</div>
+					<div class='item second-face'>
+						<span class='pip'></span>
+						<span class='pip'></span>
+					</div>
+					<div class='item third-face'>
+						<span class='pip'></span>
+						<span class='pip'></span>
+						<span class='pip'></span>
+					</div>
+					<div class='item fourth-face'>
+						<div class='column'>
+							<span class='pip'></span>
+							<span class='pip'></span>
+						</div>
+						<div class='column'>
+							<span class='pip'></span>
+							<span class='pip'></span>
+						</div>
+					</div>
+					<div class='item fifth-face'>
+						<div class='column'>
+							<span class='pip'></span>
+							<span class='pip'></span>
+						</div>
+						<div class='column'>
+							<span class='pip'></span>
+						</div>
+						<div class='column'>
+							<span class='pip'></span>
+							<span class='pip'></span>
+						</div>
+					</div>
+					<div class='item sixth-face'>
+						<div class='column'>
+							<span class='pip'></span>
+							<span class='pip'></span>
+							<span class='pip'></span>
+						</div>
+						<div class='column'>
+							<span class='pip'></span>
+							<span class='pip'></span>
+							<span class='pip'></span>
+						</div>
+					</div>
+				</div>
+			</body>
+		</div>
+	)
 }
 
-export default App;
+export default App
